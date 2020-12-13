bf2f8c6704666463a18c4e5afe73b9dd0a0bafd4
Magdaleno <Magdaleno0100@gmail.com>
2020-12-13 01:05:02 -0600

    Se agrega proyecto dotnet primera clase de arrays

---
+++ Collections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Collections
+{
+
+    public class ClaseArray
+    {
+        //Declarando arrays
+        int[] a0;
+        int[] a1 = new int[2];
+        int[] a2 = new int[2] 
+        {
+            1,2
+        };
+        int[] a3 = new int[]{
+            1,2,3,4,5
+        };
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Collections 
+
+En este repo, subiré algunos ejemplos del curso de "Beginning C# Collections"
+
+### Notas y elementos clave
+
+1. Los arrays son de tipo "Zero-indexed", lo que significa que el primer elemento del arreglo tiene la posición 0, y con esto podemos deducir que el número de index que contiene nuestro arreglo es "el número de elementos del arreglo menos 1".
+2. 
+
