3da5ba87102634131c4ea1aaa0d781266c6aa7f7
Magdaleno <Magdaleno0100@gmail.com>
2020-12-13 11:48:16 -0600

    Se agregan ejemplos de la definición de array y el uso de List<T>

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -16,13 +17,56 @@ namespace Collections
             1,2,3,4,5
         };
 
+        //Collection Generic : List<T>
+        List<int> lista = new List<int>();
+        
+
+    }
+
+    public class TopTenPops
+    {
+        string filePath = @"";
+   
+    }
+
+    public class CatModel
+    {
+        //private Guid _id;
+        public Guid Id 
+        {
+            //set { _id = value; }
+            get { return Guid.NewGuid(); }
+        }
+        public string Name {get;set;}
+        public int Year {get;set;}
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-           
+
+            string  texto = "'Egypt, Arab Rep.',EGY,Africa,97553151";
+            //string[] partes  = texto.Split(',');
+            string[] partes  = texto.Split(new char[]{','});
+            Console.WriteLine(partes.Length);
+
+            List<CatModel> listaGato  = new List<CatModel>();
+            listaGato.Add(
+                new CatModel {Name = "Cat1", Year=1}
+            );
+            listaGato.Add(
+                new CatModel {Name = "Cat2", Year=1}
+            );
+            listaGato.Add(
+                new CatModel {Name = "Cat3", Year=1}
+            );
+
+            var valorGato = listaGato.Find(x=>x.Name == "Cat2");
+        
+            System.Console.WriteLine(valorGato.Id);
+
+ 
             
         }
     }
--- README.md
+++ README.md
@@ -5,5 +5,11 @@ En este repo, subiré algunos ejemplos del curso de "Beginning C# Collections"
 ### Notas y elementos clave
 
 1. Los arrays son de tipo "Zero-indexed", lo que significa que el primer elemento del arreglo tiene la posición 0, y con esto podemos deducir que el número de index que contiene nuestro arreglo es "el número de elementos del arreglo menos 1".
+
+
+
+Comandos a tomar en cuenta:
+
+1. dotnet add package Config.Net --version 4.15.0  
 2. 
 
